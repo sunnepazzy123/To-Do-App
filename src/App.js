ea00b61d480301d902ea4a5ff84255dab5850626
sunny odibo <sunnepazzy123@gmail.com>
2020-03-27 01:52:10 -0700

    Firts Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,140 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ListItems from "./Listitems";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+library.add(faTrash);
+
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: ""
+      }
+    }
+
+    // this.deleteItem = this.deleteItem.bind(this);
+    // this.addItem = this.addItem.bind(this);
+    // this.handleInput = this.handleInput.bind(this);
+
+
+
+
+  }
+
+  handleInput = (e)=>{
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }   
+    });
+  }
+
+
+  addItem = (e)=>{
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    // console.log(newItem)
+    if(newItem.text !==""){
+      const newItems = [...this.state.items, newItem]
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: "",
+          key: ""
+        }
+      });  
+    }else{
+
+    alert("Please Enter Input")
+  }
+
+
+
+
+
+
+  }
+
+  // addItem(e){
+  //   e.preventDefault();
+  //   const newItem = this.state.currentItem;
+  //   if(newItem.text !==""){
+  //     const items = [...this.state.items, newItem];
+  //   this.setState({
+  //     items: items,
+  //     currentItem:{
+  //       text:'',
+  //       key:''
+  //     }
+  //   })
+  //   }
+  // }
+
+
+  deleteItem = (key) =>{
+    const filteredItems = this.state.items.filter( item => item.key !== key);
+    this.setState({
+      items: filteredItems
+    });
+  }
+
+  setUpdateItem = (text, key)=> {
+    const items = this.state.items;
+    // console.log(items)
+    items.map( item =>{
+
+      if(item.key === key){
+        item.text = text;
+      }
+
+    })
+    
+    this.setState({
+      items: items
+    });
+
+  }
+
+  render(){
+    return <div className="App-bg">
+     
+    
+
+<div className="App">
+   
+ <header>
+ <form id="to-do-form" onSubmit={this.addItem}>
+   <input type="text" placeholder="Enter Text" value={this.state.currentItem.text} onChange={this.handleInput} />          
+   <button type="submit">Add</button>
+ </form>
+</header>
+
+<ListItems items = {this.state.items} deleteItem ={this.deleteItem} item={this.state.currentItem.item} setUpdateItem ={this.setUpdateItem} >
+
+ </ListItems>
+
+
+</div>
+
+</div>
+
+
+
+
+    
+    
+  
+  }
+  
 }
 
 export default App;
